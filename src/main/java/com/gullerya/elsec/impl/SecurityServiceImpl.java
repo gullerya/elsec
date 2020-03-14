c40f3201ccad4c5b1b9bf5de6270f4a480d1e0df
gullerya <gullerya@gmail.com>
2020-03-14 19:15:36 +0200

    initial commit

--- src/main/java/com/gullerya/elsec/impl/SecurityServiceImpl.java
+++ src/main/java/com/gullerya/elsec/impl/SecurityServiceImpl.java
@@ -21,7 +21,7 @@ class SecurityServiceImpl implements SecurityService {
 
 	@Override
 	public SecuritySession authenticate(HttpServletRequest request) throws Exception {
-		SecuritySession securitySession = sessionsManager.obtainSession(request);
+		//  SecuritySession securitySession = sessionsManager.obtainSession(request);
 		//  put context on request
 		return null;
 	}
